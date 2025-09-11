7b09362ba6bc7a3c592653b03d2fa2da738f385e
unknown <Administrador@SANTAMARIA.LOCAL>
2025-09-11 14:35:33 -0500

    first commit

---
+++ src/main/java/com/speedRoller/aplicacion/Controller/indexController.java
@@ -0,0 +1,22 @@
+package com.speedRoller.aplicacion.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+
+@Controller
+@RequestMapping("/")
+public class indexController {
+    
+    @GetMapping("home")
+    public String Home(){
+        return "index";
+    }
+    
+
+}
+
+//localhost:8085/index
