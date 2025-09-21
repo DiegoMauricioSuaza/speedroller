643331703acd64d1cbaf80c2c2ea0144bfee9ca3
unknown <Administrador@SANTAMARIA.LOCAL>
2025-09-21 14:54:23 -0500

    Tu mensaje descriptivo del commit

---
+++ src/main/java/com/speedRoller/aplicacion/Repository/userRepository.java
@@ -0,0 +1,14 @@
+package com.speedRoller.aplicacion.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import Model.userModel;
+
+
+@Repository
+public interface userRepository extends JpaRepository<userModel, Long>
+{
+
+    
+}
--- src/main/java/com/speedRoller/aplicacion/Service/userService.java
+++ src/main/java/com/speedRoller/aplicacion/Service/userService.java
@@ -1,15 +1,21 @@
 package com.speedRoller.aplicacion.Service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.stereotype.Service;
 import com.speedRoller.aplicacion.Repository.UserRepository;
 
+import Model.userModel;
+
 @Service
 public class userService {
     
+    @Autowired
     UserRepository userRepository;
 
-    public listarUser() {
+    public List<userModel> listarUser() {
         return userRepository.findAll();
     }
     
