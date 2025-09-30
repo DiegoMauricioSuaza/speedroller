46c9f6f3a9aaa7cda1f2316660103fb6cfff2628
unknown <Administrador@SANTAMARIA.LOCAL>
2025-09-30 09:16:30 -0500

    Tu mensaje de commit aquí

--- src/main/java/com/speedRoller/aplicacion/Repository/userRepository.java
+++ src/main/java/com/speedRoller/aplicacion/Repository/userRepository.java
@@ -1,12 +1,9 @@
-package com.speedRoller.aplicacion.Repository;
 
 package com.speedroller.aplicacion.Repository;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.speedroller.Model.userModel;
-
-import Model.userModel;
+import com.speedroller.aplicacion.Model.userModel;
 
 @Repository
 public interface UserRepository extends JpaRepository<userModel, Long> {
